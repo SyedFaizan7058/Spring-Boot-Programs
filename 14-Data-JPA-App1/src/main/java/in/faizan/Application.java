fc696a1fb8f705aa40a6ad43bbb216f0e702aeb8
SyedFaizan7058 <157882406+SyedFaizan7058@users.noreply.github.com>
2024-07-18 12:34:22 +0530

    Add files via upload

---
+++ 14-Data-JPA-App1/src/main/java/in/faizan/Application.java
@@ -0,0 +1,32 @@
+package in.faizan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import in.faizan.service.EmpService;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+		EmpService empService = context.getBean(EmpService.class);
+
+//		empService.saveEmployee();
+
+		// empService.saveEmps();
+
+		// empService.getEmp(201);
+
+		// empService.getEmps(Arrays.asList(101,102,201));
+
+		// empService.getEmps();
+
+		// empService.callFindByMethods();
+
+//		empService.callCustomQueries();
+	}
+
+}
